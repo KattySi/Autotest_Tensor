4bfe29ea4f866589d8177fe2ab040c5cfba78b33
KattySi <katerina.utg@gmail.com>
2025-02-28 21:43:15 +0300

    add options for download to fixture browser

--- conftest.py
+++ conftest.py
@@ -5,18 +5,20 @@ import shutil
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 
 
 DOWNLOAD_DIR: Path = Path.cwd() / 'download/'
 
 @pytest.fixture(scope='module')
 def browser():
-    download_in_directory: Path = Path.cwd() / 'download'
     chrome_options: Options = Options()
     chrome_options.add_experimental_option(
         "prefs", {
-        "download.default_directory": str(download_in_directory),
+        "download.default_directory": str(DOWNLOAD_DIR),
+        "download.prompt_for_download": False,
+        "safebrowsing.allow_downloads_from_hosts": "https://sbis.ru/",
+        "safebrowsing.enabled": True,
+        "download.directory_upgrade": True,
     })
     browser: WebDriver = webdriver.Chrome(options=chrome_options)
     yield browser
