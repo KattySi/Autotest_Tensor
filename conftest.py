89069611c2688ebebbb1244c18d2b036dd1b1bbd
KattySi <katerina.utg@gmail.com>
2025-02-28 21:14:39 +0300

    Delete path to chromedriver and service from browser fixture

--- conftest.py
+++ conftest.py
@@ -8,21 +8,17 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 
-
 DOWNLOAD_DIR: Path = Path.cwd() / 'download/'
 
 @pytest.fixture(scope='module')
 def browser():
-    chrome_driver_path: str = '/usr/local/bin/chromedriver'
     download_in_directory: Path = Path.cwd() / 'download'
     chrome_options: Options = Options()
     chrome_options.add_experimental_option(
         "prefs", {
         "download.default_directory": str(download_in_directory),
     })
-
-    service: Service = Service(chrome_driver_path)
-    browser: WebDriver = webdriver.Chrome(options=chrome_options, service=service)
+    browser: WebDriver = webdriver.Chrome(options=chrome_options)
     yield browser
     browser.quit()
 
