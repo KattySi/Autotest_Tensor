bb3306d686b829c235c616f746cc45ee5b3bd20a
KattySi <katerina.utg@gmail.com>
2025-02-28 15:19:04 +0300

    delete sleep, assert new_region test_scenario_2.py

--- tests/test_scenario_2.py
+++ tests/test_scenario_2.py
@@ -19,6 +19,7 @@ def test_two(browser: webdriver) -> None:
 
     list_partners: WebElement = contacts_page.should_be_list_partners()
     assert list_partners.text == "Нижний Новгород", "Не правильный список партнеров"
+
     region.click()
     kamchatka: WebElement = contacts_page.should_be_kamchatka()
     kamchatka.click()
@@ -26,10 +27,11 @@ def test_two(browser: webdriver) -> None:
 
     new_region: WebElement = contacts_page.should_be_region()
     new_list_partners: WebElement = contacts_page.should_be_list_partners()
-    assert new_region.text == "Камчатский край", "Не Камчатский Край"
     assert new_list_partners.text == "Петропавловск-Камчатский", "Список партнеров не Камчатка"
+
     new_url: str = browser.current_url
     assert "41-kamchatskij-kraj" in new_url, "В адресе нет Камчатского края"
+
     page_title: str = browser.title
     assert "Камчатский край" in page_title, "В тексте заголовка нет Камчатского края"
 
